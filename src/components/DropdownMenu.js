dc91f2dbe35bb426536dcf18c23593f1a18a1f9e
Ntami Egbe <ntamiegbe00@gmail.com>
2023-04-04 16:06:36 +0100

    Commit: changes

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,49 +1,68 @@
-import { useState } from "react";
 import { BiChevronDown, BiLogOut } from 'react-icons/bi'
 import { RiUserLine } from 'react-icons/ri'
 import { TbSteeringWheel } from 'react-icons/tb'
 import Logo from '../assets/person.png'
 import Image from 'next/image'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
+
 
 const DropdownMenu = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div className="relative">
-            <button className="flex items-center space-x-3 px-10 py-2 text-sm font-medium text-gray-700 bg-white rounded-md focus:outline-none" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                <Image src={Logo} alt='profile image' className='object-contain rounded-full w-10 h-10' />
-                <BiChevronDown className='hidden md:inline-flex h-5 w-5 text-icons' />
-            </button>
-
-            {isMenuOpen && (
-                <div className="absolute z-10 mt-5 origin-top-right rounded-md shadow-lg bg-white w-auto">
-                    <div className="py-5 rounded-md shadow-xs">
-                        <a
-                            href="/userProfile"
-                            className="flex items-center space-x-2 px-4 py-2 text-secondaryText hover:bg-overlay hover:text-brand hover:font-medium rounded-lg mx-2"
-                        >
-                            <RiUserLine />
-                            <p className="text-base">Profile</p>
-                        </a>
-                        <a
-                            href="/vehicles"
-                            className="flex items-center space-x-2 px-4 py-2 text-secondaryText hover:bg-overlay hover:text-brand hover:font-medium rounded-lg mx-2"
-                        >
-                            <TbSteeringWheel />
-                            <p className="text-base">My vehicles</p>
-                        </a>
-                        <a
-                            href="/"
-                            className="flex items-center space-x-2 px-4 py-2 text-secondaryText hover:bg-overlay hover:text-brand hover:font-medium rounded-lg mx-2"
-                        >
-                            <BiLogOut />
-                            <p className="text-base">Log out</p>
-                        </a>        
-                    </div>
+        <div className="z-50">
+            <Menu as="div" className="relative inline-block text-left">
+                <div>
+                    <Menu.Button className="flex items-center w-full space-x-3 text-sm font-medium text-gray-700 bg-white rounded-md focus:outline-none">
+                        <Image src={Logo} alt='profile image' className='object-contain rounded-full w-10 h-10' />
+                        <BiChevronDown className='hidden md:inline-flex h-5 w-5 text-icons' />
+                    </Menu.Button>
                 </div>
-            )}
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow focus:outline-none">
+                        <div className="px-2 py-4">
+                            <Menu.Item>
+                                <Link href="/userProfile"
+                                    className="w-full flex items-center space-x-2 px-4 py-2 text-secondaryText hover:bg-overlay hover:text-brand hover:font-medium rounded-lg"
+                                >
+                                    <RiUserLine />
+                                    <p className="text-base">Profile</p>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link href="/vehicles"
+                                    className="w-full flex items-center space-x-2 px-4 py-2 text-secondaryText hover:bg-overlay hover:text-brand hover:font-medium rounded-lg"
+                                >
+                                    <TbSteeringWheel />
+                                    <p className="text-base">My vehicles</p>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link href="/"
+                                    className="w-full flex items-center space-x-2 px-4 py-2 text-secondaryText hover:bg-overlay hover:text-brand hover:font-medium rounded-lg"
+                                >
+                                    <BiLogOut />
+                                    <p className="text-base">Log out</p>
+                                </Link>
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
         </div>
-    );
+    )
 };
 
 export default DropdownMenu;
+
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { BiChevronDown } from 'react-icons/bi'
 import DropdownMenu from './DropdownMenu'
+import Link from 'next/link'
 
 const Navbar = ({page}) => {
     return (
@@ -7,7 +8,7 @@ const Navbar = ({page}) => {
             <div className="flex justify-between items-center p-5">
                 <h2 className='text-primary text-lg font-medium'>{page}</h2>
                 <div className="flex items-center space-x-6">
-                    <p className='hidden md:inline-flex text-secondaryText text-base font-normal'>Verify</p>
+                    <Link href="/verify" className='hidden md:inline-flex text-secondaryText text-base font-normal'>Verify</Link>
                     <p className='hidden md:inline-flex text-secondaryText text-base font-normal'>Report missing vehicle</p>
                     <svg width="19" height="15" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.4417 17.5C10.2952 17.7525 10.0849 17.9622 9.83185 18.1079C9.57884 18.2536 9.29198 18.3303 9 18.3303C8.70802 18.3303 8.42116 18.2536 8.16814 18.1079C7.91513 17.9622 7.70484 17.7525 7.55833 17.5M14 6.66663C14 5.34054 13.4732 4.06877 12.5355 3.13109C11.5979 2.19341 10.3261 1.66663 9 1.66663C7.67392 1.66663 6.40215 2.19341 5.46447 3.13109C4.52678 4.06877 4 5.34054 4 6.66663C4 12.5 1.5 14.1666 1.5 14.1666H16.5C16.5 14.1666 14 12.5 14 6.66663Z" stroke="#8593AF" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
