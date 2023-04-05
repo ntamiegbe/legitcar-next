92791d6cd091687ab0843a7c88c90e58936c2802
Ntami Egbe <ntamiegbe00@gmail.com>
2023-04-05 13:06:31 +0100

    Commit: chnages

--- src/components/VehicleDetails.js
+++ src/components/VehicleDetails.js
@@ -109,7 +109,7 @@ const VehicleDetails = ({ carMake, VIN, startDate, completeDate, plan, status, r
                                     <div className="bg-orangeOverlay rounded-full px-4 py-3 flex items-center justify-center">
                                         <p className='text-orange text-center font-semibold text-4xl'>M</p>
                                     </div>
-                                    <div className="w-[24rem] lg:w-auto px-4">
+                                    <div className="max-w-[24rem] lg:w-auto px-4">
                                         <p className='text-start text-primary text-base font-normal mb-5'>{repairShop}</p>
                                         <div className="flex items-center space-x-2 mb-4">
                                             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
